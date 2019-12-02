6997caa5543779369b8046a27c013605266d84b8
James Bungard <jmbungard@gmail.com>
2019-12-02 17:55:25 +1100

    2018 day 1

---
+++ aoc18/1.py
@@ -0,0 +1,29 @@
+def day1():
+    data = []
+    with open("input/1.in") as inp:
+        for line in inp:
+            data.append(int(line))
+
+    # Part 1
+    freq = sum(data)
+    print(f"Part 1: {freq}")
+
+    # Part 2
+    freq = 0
+    seen = set([freq])
+    done = False
+    while not done:
+        for datum in data:
+            freq += datum
+            if freq in seen:
+                done = True
+                break
+            seen.add(freq)
+
+    print(f"Part 1: {freq}")
+
+
+
+
+if __name__ == "__main__":
+    day1()
